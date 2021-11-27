ee3d7315a5c70a1dd6c3e522aa763bc74f4a23b6
Veepro <aba-naz@yandex.ru>
2021-11-27 18:57:37 +0300

    added information about launch

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,3 +1,8 @@
+/*
+Before run Main use Generator for generation
+Generator create 3 file (.txt containing array of nums)
+*/
+
 public class Main {
     public static void main(String[] args) {
         new GUI();
